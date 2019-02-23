9b5e651bd0bd06ed6b11c1e568488770a519778c
cerihum <47921920+cerihum@users.noreply.github.com>
2019-02-23 18:01:08 +0000

    Update metadata.js

--- data/metadata.js
+++ data/metadata.js
@@ -2,10 +2,10 @@ var models = [
 
 
     {width: 30, height: 22, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0000.jpg", answer: "to"}, 
-    {width: 29, height: 24, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0001.jpg"}, 
-    {width:24, height: 26, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0002.jpg"}, 
-    {width: 24, height: 21, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0003.jpg"}, 
-    {width: 46, height: 19, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0004.jpg"},
+    {width: 29, height: 24, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0001.jpg", answer: "favorable"}, 
+    {width:24, height: 26, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0002.jpg", answer: "tried"}, 
+    {width: 24, height: 21, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0003.jpg", answer: "may"}, 
+    {width: 46, height: 19, dir: 1, file: "data/TNA_CCC_HO13_001_00004.0004.jpg", answer: "Condition"},
 
 
 ];
